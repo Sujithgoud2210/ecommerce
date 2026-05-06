85e3a0edee2d7a14dee787fbd62f5c9dd13e3aaf
sujithgoud2210 <sujith200322@gmail.com>
2026-05-06 10:54:53 +0530

    enums

---
+++ ecommerce/src/main/java/com/stschool/ecommerce/enums/Membership.java
@@ -0,0 +1,8 @@
+package com.stschool.ecommerce.enums;
+
+public enum Membership {
+    BRONZE,
+    SILVER,
+    GOLD,
+    DIAMOND
+}
---
+++ ecommerce/src/main/java/com/stschool/ecommerce/enums/Status.java
@@ -0,0 +1,7 @@
+package com.stschool.ecommerce.enums;
+
+public enum Status {
+    ACTIVE,
+    INACTIVE,
+    BLOCKED
+}
