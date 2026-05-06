6f9c08813a105eb26e2542ca83c5e4a1260450c8
sujithgoud2210 <sujith200322@gmail.com>
2026-05-06 10:52:13 +0530

    controller

--- ecommerce/src/main/java/com/stschool/ecommerce/controller/AuthController.java
+++ ecommerce/src/main/java/com/stschool/ecommerce/controller/AuthController.java
@@ -1,4 +1,36 @@
 package com.stschool.ecommerce.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) throws IOException {
+        this.authService = authService;
+    }
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody Customer customer)  {
+        try {
+            Customer signedUpCustomer = authService.signup(customer);
+            return ResponseEntity.status(HttpStatus.CREATED).body(signedUpCustomer);
+        } catch (CustomerExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        } catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginCredentials loginCredentials) {
+        try {
+            return ResponseEntity.ok(authService.login(loginCredentials.getEmail(), loginCredentials.getPassword()));
+        } catch (CustomerNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+    public Customer getCustomerByEmail(String email) throws CustomerNotFoundException {
+        return authService.getCustomerByEmail(email);
+    }
 }
--- ecommerce/src/main/java/com/stschool/ecommerce/controller/CustomerController.java
+++ ecommerce/src/main/java/com/stschool/ecommerce/controller/CustomerController.java
@@ -1,4 +1,40 @@
 package com.stschool.ecommerce.controller;
 
 public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) throws IOException {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/{id}")
+    //api/v1/customers/10
+    public ResponseEntity<?> getCustomerById(@PathVariable int id)  {
+        try {
+            return ResponseEntity.ok(customerService.getById(id));
+        } catch (CustomerNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch(Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getAllCustomers() {
+        return ResponseEntity.ok(customerService.getAllCustomers());
+    }
+
+    public Customer getCustomerByEmail(String email) throws CustomerNotFoundException {
+        return customerService.getByEmail(email);
+    }
+
+    public Customer updateCustomer(Customer customer) throws Exception {
+        return customerService.updateCustomer(customer);
+    }
+
+    @DeleteMapping("/")
+    //api/v1/customers?id=1
+    public void deleteCustomer(@RequestParam("id") int id) throws Exception {
+        customerService.deleteCustomer(id);
+    }
 }
